74acba1256bb116cea3d1971a102f717754f8ea8
JorgeAsca <jascola171@g.educaand.es>
2025-03-11 18:02:38 -1100

    Añadiendo los productos a la pagina

--- src/main/java/com/jj/tienda/Controllers/SaborController.java
+++ src/main/java/com/jj/tienda/Controllers/SaborController.java
@@ -1,20 +1,28 @@
 package com.jj.tienda.Controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.jj.tienda.Models.Repository.SaboresRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jj.tienda.Models.Entity.Sabores;
+import com.jj.tienda.Models.Services.SaboresServices;
+
 
 @Controller
+@RequestMapping("/sabores")
 public class SaborController {
 
     @Autowired
-    private SaboresRepository saborRepository;
+    private SaboresServices gamasService;
 
-    @GetMapping("/sabores")
-    public String listarSabores(Model model) {
-        model.addAttribute("sabores", saborRepository.findAll());
-        return "sabores"; // Thymeleaf buscará "sabores.html"
+    @GetMapping("/index")
+    public String ListadeSabores(Model model) {
+        List<Sabores> sabores = gamasService.getAll();
+        model.addAttribute("sabores", sabores);
+        return "www/sabores/index";
     }
 }
--- src/main/java/com/jj/tienda/Models/Entity/Sabores.java
+++ src/main/java/com/jj/tienda/Models/Entity/Sabores.java
@@ -12,6 +12,47 @@ public class Sabores {
 
     private String nombre;
     private String descripcion;
+    private String imagen;
+    private Double precio;
 
     // Getters y Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
 }
