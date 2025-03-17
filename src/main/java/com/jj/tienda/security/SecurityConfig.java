892a5692c4dbd13f3d77a3ec24ea8c7728e40441
JorgeAsca <jascola171@g.educaand.es>
2025-03-16 13:15:43 -1100

    Solucioado el error de el login

--- src/main/java/com/jj/tienda/security/SecurityConfig.java
+++ src/main/java/com/jj/tienda/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
             )
             .logout(logout -> logout
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout")
+                .logoutSuccessUrl("/")
                 .permitAll()
             );
 
